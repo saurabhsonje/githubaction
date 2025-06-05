b6e08fb287a06e0354d6aac085e01427a660f6d6
saurabhsonje <50398982+saurabhsonje@users.noreply.github.com>
2025-06-06 01:14:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,34 @@
+ provider "aws" {
+   region = ""
+ }
 
+ resource "aws_iam_role" "lambda_role" {
+   name = "lambda_role"
+   assume_role_policy = jsonencode({
+     Version = "2012-10-17"
+     Statement = [
+       {
+         Action = "sts:AssumeRole"
+         Effect = "Allow"
+         Principal = {
+           Service = "lambda.amazonaws.com"
+         }
+       }
+     ]
+   })
+ }
+
+ resource "aws_iam_role_policy_attachment" "lambda_policy" {
+   role       = aws_iam_role.lambda_role.name
+   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+ }
+
+ resource "aws_lambda_function" "my_lambda" {
+   function_name = "my_lambda_function"
+   role          = aws_iam_role.lambda_role.arn
+   handler       = "index.handler"
+   runtime       = "nodejs18.x"
+   filename      = "lambda_function_payload.zip"
+   source_code_hash = filebase64sha256("lambda_function_payload.zip")
+ }
+ ```
