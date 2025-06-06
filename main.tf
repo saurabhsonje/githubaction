8853447863527cceb4c7701884b432639b60c85d
saurabhsonje <50398982+saurabhsonje@users.noreply.github.com>
2025-06-06 14:26:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,6 @@
    role          = aws_iam_role.lambda_role.arn
    handler       = "index.handler"
    runtime       = "nodejs18.x"
-   #filename      = "lambda_function_payload.zip"
-   #source_code_hash = filebase64sha256("lambda_function_payload.zip")
+   filename      = "lambda_function_payload.zip"
+   source_code_hash = filebase64sha256("lambda_function_payload.zip")
  }
