38724613a22524960c24e8660dae41376123be5f
saurabhsonje <50398982+saurabhsonje@users.noreply.github.com>
2025-06-06 17:15:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,8 +27,7 @@
  resource "aws_lambda_function" "my_lambda" {
    function_name = "nodejs_lambda_function"
    role          = aws_iam_role.lambda_role.arn
-   handler       = "index.handler"
-   runtime       = "nodejs18.x"
-   filename      = "lambda_function_payload.zip"
-   source_code_hash = filebase64sha256("lambda_function_payload.zip")
+   package_type  = "Image"
+   image_uri      = "810471244058.dkr.ecr.eu-north-1.amazonaws.com/nodejs/githubaction:latest"
+   
  }
