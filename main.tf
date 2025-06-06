8bd762e214ca2dc70dad43061b37721e23896a79
saurabhsonje <50398982+saurabhsonje@users.noreply.github.com>
2025-06-06 17:20:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
  }
 
  resource "aws_iam_role" "lambda_role" {
-   name = "lambda_role_new"
+   name = "lambda_role_check"
    assume_role_policy = jsonencode({
      Version = "2012-10-17"
      Statement = [
