a241eea493b734ba5aa56e3d5262c5318e8ff601
saurabhsonje <50398982+saurabhsonje@users.noreply.github.com>
2025-06-06 15:48:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,6 @@
  }
 
 
- resource "aws_iam_role_policy_attachment" "lambda_policy" {
-   role       = aws_iam_role.lambda_role.name
-   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
- }
 
  resource "aws_lambda_function" "my_lambda" {
    function_name = "nodejs_lambda_function"
